3502c326b9283c7728d2031793762dfaed35dfbc
Just-a-developer-abhi <abhi344741@gmail.com>
2024-03-29 22:58:47 +0530

    Navbar in progress

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { styles } from '../styles'
+import { logo, menu, close } from '../assets'
+import { navLinks } from '../constants'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+  const [active, setActive] = useState("")
   return (
-    <div>Navbar</div>
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary` }>
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link 
+        to="/" c
+        lassName='flex items-center gap-2' 
+        onClick={()=>{
+          setActive("");
+          window.scrollTo(0, 0);
+        }}>
+          <img src={logo} alt='logo' className='w-9 h-9 object-contain'/>
+        </Link>
+      </div>
+    </nav>
   )
 }
 
